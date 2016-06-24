8d02bf04298b18807e54a43adf412846bbd3c904
Jeremy M. Reed <reeje76@gmail.com>
2016-06-24 12:01:03 -0600

    Add NameTracker class.
    Stub out methods and unit tests.

---
+++ src/main/java/utilities/NameTracker.java
@@ -0,0 +1,29 @@
+package utilities;
+
+import java.util.Map;
+
+import exceptions.NotImplementedException;
+
+public class NameTracker
+{
+
+	private static Map<String, Boolean> nameMap;
+	
+	/**
+	 * Initialize the name map.
+	 */
+	public NameTracker() {
+		throw new NotImplementedException();
+	}
+	
+	/**
+	 * Checks to see if the name already exists in the map.
+	 * If the name exists in the map, return true.
+	 * If the name does not exist in the map, add it, and return false.
+	 * @param name
+	 * @return
+	 */
+	public boolean isNameUsed(String name) {
+		throw new NotImplementedException();
+	}
+}
---
+++ src/test/java/utilities/NameTrackerTest.java
@@ -0,0 +1,33 @@
+package utilities;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import exceptions.NotImplementedException;
+
+public class NameTrackerTest
+{
+
+	@Test
+	public void test_isNameUsed_EmptyMap_ValidName_ReturnsFalse( ) {
+		throw new NotImplementedException();
+	}
+
+	@Test
+	public void test_isNameUsed_NonEmptyMap_ValidNameInMap_ReturnsTrue() {
+		throw new NotImplementedException();
+	}
+	
+	@Test
+	public void test_isNameUsed_EmptyMap_EmptyName_ThrowsException() {
+		throw new NotImplementedException();
+	}
+	
+	@Test
+	public void test_isNameUsed_EmptyMap_NullName_ThrowsException() {
+		throw new NotImplementedException();
+	}
+}
