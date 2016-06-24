6c1fb442cf003a4dc9d706aabc736c83e572ce8c
Jeremy M. Reed <reeje76@gmail.com>
2016-06-24 12:05:14 -0600

    Implement NameTracker unit tests.

--- src/test/java/utilities/NameTrackerTest.java
+++ src/test/java/utilities/NameTrackerTest.java
@@ -2,32 +2,49 @@ package utilities;
 
 import static org.junit.Assert.*;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import java.security.InvalidParameterException;
 
-import exceptions.NotImplementedException;
+import org.junit.Test;
 
 public class NameTrackerTest
 {
 
 	@Test
 	public void test_isNameUsed_EmptyMap_ValidName_ReturnsFalse( ) {
-		throw new NotImplementedException();
+		NameTracker tracker = new NameTracker();
+		assertFalse( tracker.isNameUsed( "Foo" ) );
 	}
 
 	@Test
 	public void test_isNameUsed_NonEmptyMap_ValidNameInMap_ReturnsTrue() {
-		throw new NotImplementedException();
+		NameTracker tracker = new NameTracker();
+		tracker.isNameUsed( "Foo" );
+		assertTrue( tracker.isNameUsed( "Foo" ) );
 	}
 	
 	@Test
 	public void test_isNameUsed_EmptyMap_EmptyName_ThrowsException() {
-		throw new NotImplementedException();
+		try {
+			NameTracker tracker = new NameTracker();
+			tracker.isNameUsed( "" );
+			fail("Supposed to catch InvalidParameterException");
+		} catch ( InvalidParameterException ipe ) {
+			
+		} catch ( Exception e ) {
+			fail("Expected to catch InvalidParameterException");
+		}
 	}
 	
 	@Test
 	public void test_isNameUsed_EmptyMap_NullName_ThrowsException() {
-		throw new NotImplementedException();
+		try {
+			NameTracker tracker = new NameTracker();
+			tracker.isNameUsed( null );
+			fail("Supposed to catch NullPointerException");
+		} catch ( NullPointerException npe ) {
+			
+		} catch ( Exception e ) {
+			fail("Expected to catch NullPointerException");
+		}
 	}
 }
